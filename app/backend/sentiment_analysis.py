0e231471eb2bd3093f3155e5f4a6ca86a2480854
Manya Singh <singhmanya0810@gmail.com>
2021-12-05 17:21:58 -0500

    sentiment analysis functional code

--- app/backend/sentiment_analysis.py
+++ app/backend/sentiment_analysis.py
@@ -3,6 +3,8 @@ import urllib.request
 from transformers import pipeline
 from indexer import Indexer
 from googletrans import Translator
+from deep_translator import GoogleTranslator
+
 
 class SentimentAnalysis:
     def __init__(self):
@@ -10,14 +12,14 @@ class SentimentAnalysis:
         self.translator = Translator()
 
     def perform_sentiment_analysis(self):
-        solr_query = "http://ec2-52-72-185-101.compute-1.amazonaws.com:8983/solr/BM25_Project_4/select?q.op=OR&q=*%3A*&rows=461761"
-        #461761
+        solr_query = "http://ec2-52-72-185-101.compute-1.amazonaws.com:8983/solr/BM25_Project_4/select?q.op=OR&q=*%3A*&rows=461761&start=1239"
         data = urllib.request.urlopen(solr_query)
         docs = json.load(data)['response']['docs']
-        final_documents = []
+        indexer = Indexer()
         for document in docs:
-            text_en = self.translator.translate(document["tweet_text"], dest='en').text
-            if len(text_en) > 512:
+            text_en = document["tweet_text"]
+            text_en = GoogleTranslator(source='auto', target='en').translate(text_en)
+            if (len(text_en) > 512):
                 text_en = text_en[:512]
             classifier_result = self.classifier(text_en)
             sentiment = classifier_result[0]['label']
@@ -25,9 +27,7 @@ class SentimentAnalysis:
             document['sentiment'] = sentiment
             document['sentiment_score'] = sentiment_score
             document.pop('_version_')
-            final_documents.append(document)
-        indexer = Indexer()
-        indexer.create_documents(final_documents)
+            indexer.create_documents(document)
 
 if __name__ == "__main__":
     sentiment_analysis = SentimentAnalysis()
